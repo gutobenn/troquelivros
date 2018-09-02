767c9ac1a6f58f7c10a3ba13d07bc3c1cb7aa767
Augusto Bennemann <gutobenn@gmail.com>
2018-09-01 23:26:25 -0400

    Geolocate user on profile update

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902023934) do
+ActiveRecord::Schema.define(version: 20180902032231) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "name"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.bigint "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.string "addr_street"
-    t.string "addr_number"
-    t.string "addr_complement"
-    t.string "addr_city"
-    t.string "phone"
-    t.string "addr_state"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'foat' for column 'longitude'
 
 end
