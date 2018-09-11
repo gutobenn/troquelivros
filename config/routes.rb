44953e02aa247a81aed276329a234cfc7b5bb25d
Augusto Bennemann <gutobenn@gmail.com>
2018-09-10 23:31:19 -0400

    Put registrations controller back to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :books
   resources :book_instances
   root to: 'visitors#index'
-  #devise_for :users, controllers: { registrations: 'registrations' }
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users
 end
