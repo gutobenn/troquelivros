02d0727e86df2feab68e64253543ea220c3eebe3
Fabricio Mazzola <fabriciommazzola@gmail.com>
2018-09-26 11:14:20 -0300

    Add default image for new books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/book_missing.png"
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, :default_url => ":style/book.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   validates_presence_of :name, :author
 
