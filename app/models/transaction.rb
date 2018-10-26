8640370f55ebf90c36d38c2cf1057247e3408b94
Augusto Bennemann <gutobenn@gmail.com>
2018-10-25 22:58:38 -0400

    Fixes on transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :askingUser, :class_name => 'User'
   belongs_to :askedUser, :class_name => 'User'
   belongs_to :askedBook_instance, :class_name => 'BookInstance'
-  belongs_to :askingBook_instance, :class_name => 'BookInstance'
+  belongs_to :askingBook_instance, :class_name => 'BookInstance', optional: true
 
 end
